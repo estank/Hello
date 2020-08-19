b9353fd3b707300fd60801c567b3f8fc840c4656
estank <69942061+estank@users.noreply.github.com>
2020-08-19 19:38:16 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*****************************
+* Hello
+******************************
+* Evan Stankard
+* 8/19/20
+* CMSC 255 - 004
+*****************************/
+
+public class Hello{
+
+public static void main (String [] args){
+
+System.out.println("Evan Stankard");
+System.out.println("Hello");
+System.out.println("Prints out Student Info");
+System.out.println("JDK 13.0.2");
+System.out.println("CMSC 255 - 004");
+System.out.println();
+
+}
+
+}
